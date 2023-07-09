38e3fbab211c599b1b1470ad24c8ae2a75c78d29
Luis <rodriguesluis414@gmail.com>
2023-07-09 01:10:33 -0500

    Agrege las peliculas a sus respectivos Generos

--- src/main.js
+++ src/main.js
@@ -12,11 +12,9 @@ async function getTrendingMoviesPreview() {
   const { data } = await api("trending/movie/day")
   const movies = data.results
 
-  movies.forEach((movie) => {
-    const trendingMoviesPreviewList = document.querySelector(
-      "#trendingPreview .trendingPreview-movieList"
-    )
+  trendingMoviesPreviewList.innerHTML = "" // estamos quitando las cargas repetidas movies
 
+  movies.forEach((movie) => {
     const movieContainer = document.createElement("div")
     movieContainer.classList.add("movie-container")
 
@@ -37,21 +35,49 @@ async function getCategoriesPreview() {
   const { data } = await api("genre/movie/list")
   const categories = data.genres
 
-  categories.forEach((category) => {
-    const categoriesPreviewList = document.querySelector(
-      "#categoriesPreview .categoriesPreview-list"
-    )
+  categoriesPreviewList.innerHTML = "" //estamos quitando las cargas repetidas categorias
 
+  categories.forEach((category) => {
     const categoryContainer = document.createElement("div")
     categoryContainer.classList.add("category-container")
 
     const categoryTitle = document.createElement("h3")
     categoryTitle.classList.add("category-title")
     categoryTitle.setAttribute("id", "id" + category.id)
+    categoryTitle.addEventListener("click", () => {
+      location.hash = `#category=${category.id}-${category.name}`
+    })
     const categoryTitleText = document.createTextNode(category.name)
 
     categoryTitle.appendChild(categoryTitleText)
     categoryContainer.appendChild(categoryTitle)
     categoriesPreviewList.appendChild(categoryContainer)
+  })
+}
+
+async function getMoviesByCategory(id) {
+  const { data } = await api("discover/movie", {
+    params: {
+      with_genres: id,
+    },
+  })
+  const movies = data.results
+
+  genericSection.innerHTML = "" // estamos quitando las cargas repetidas movies
+
+  movies.forEach((movie) => {
+    const movieContainer = document.createElement("div")
+    movieContainer.classList.add("movie-container")
+
+    const movieImg = document.createElement("img")
+    movieImg.classList.add("movie-img")
+    movieImg.setAttribute("alt", movie.title)
+    movieImg.setAttribute(
+      "src",
+      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+    )
+
+    movieContainer.appendChild(movieImg)
+    genericSection.appendChild(movieContainer)
   })
 }
