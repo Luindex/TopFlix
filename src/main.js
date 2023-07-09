f16ac0b78f81552d8d50cf487cc2a4b10a687be0
Luis <rodriguesluis414@gmail.com>
2023-07-09 01:26:36 -0500

    Agrege el scrollTop a la pagina

--- src/main.js
+++ src/main.js
@@ -8,11 +8,10 @@ const api = axios.create({
   },
 })
 
-async function getTrendingMoviesPreview() {
-  const { data } = await api("trending/movie/day")
-  const movies = data.results
+//Utils
 
-  trendingMoviesPreviewList.innerHTML = "" // estamos quitando las cargas repetidas movies
+function createMovies(movies, container) {
+  container.innerHTML = ""
 
   movies.forEach((movie) => {
     const movieContainer = document.createElement("div")
@@ -27,16 +26,12 @@ async function getTrendingMoviesPreview() {
     )
 
     movieContainer.appendChild(movieImg)
-    trendingMoviesPreviewList.appendChild(movieContainer)
+    container.appendChild(movieContainer)
   })
 }
 
-async function getCategoriesPreview() {
-  const { data } = await api("genre/movie/list")
-  const categories = data.genres
-
-  categoriesPreviewList.innerHTML = "" //estamos quitando las cargas repetidas categorias
-
+function createCategories(categories, container) {
+  container.innerHTML = ""
   categories.forEach((category) => {
     const categoryContainer = document.createElement("div")
     categoryContainer.classList.add("category-container")
@@ -51,8 +46,25 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText)
     categoryContainer.appendChild(categoryTitle)
-    categoriesPreviewList.appendChild(categoryContainer)
+    container.appendChild(categoryContainer)
   })
+}
+
+// Llamados a la API
+
+async function getTrendingMoviesPreview() {
+  const { data } = await api("trending/movie/day")
+  const movies = data.results
+
+  createMovies(movies, trendingMoviesPreviewList)
+}
+
+async function getCategoriesPreview() {
+  const { data } = await api("genre/movie/list")
+  const categories = data.genres
+
+  //categoriesPreviewList //estamos quitando las cargas repetidas categorias
+  createCategories(categories, categoriesPreviewList)
 }
 
 async function getMoviesByCategory(id) {
@@ -63,21 +75,5 @@ async function getMoviesByCategory(id) {
   })
   const movies = data.results
 
-  genericSection.innerHTML = "" // estamos quitando las cargas repetidas movies
-
-  movies.forEach((movie) => {
-    const movieContainer = document.createElement("div")
-    movieContainer.classList.add("movie-container")
-
-    const movieImg = document.createElement("img")
-    movieImg.classList.add("movie-img")
-    movieImg.setAttribute("alt", movie.title)
-    movieImg.setAttribute(
-      "src",
-      "https://image.tmdb.org/t/p/w300" + movie.poster_path
-    )
-
-    movieContainer.appendChild(movieImg)
-    genericSection.appendChild(movieContainer)
-  })
+  createMovies(movies, genericSection)
 }
