4493a0ff0508a8ec910ea906aa94f4fd0ea4001d
Luis <rodriguesluis414@gmail.com>
2023-07-10 00:42:55 -0500

    Agregue la funcion de peliculas similares

--- src/main.js
+++ src/main.js
@@ -118,4 +118,12 @@ async function getMovieByid(id) {
   movieDetailScore.textContent = movie.vote_average
 
   createCategories(movie.genres, movieDetailCategoriesList)
+  getRelatedMoviesId(id)
+}
+
+async function getRelatedMoviesId(id) {
+  const { data } = await api(`movie/${id}/recommendations`)
+  const relatedMovies = data.results
+
+  createMovies(relatedMovies, relatedMoviesContainer)
 }
